1b970333e2ca584075c0b822531a24f5dfc4a4c8
FredBill1 <1781990573@qq.com>
2025-03-19 11:32:26 +0000

    update

--- quant_experiment/quant_dorefa.py
+++ quant_experiment/quant_dorefa.py
@@ -17,45 +17,57 @@ QAT_MAX_EPOCHS = 200
 def main() -> None:
     device = get_device()
 
-    model = create_model(from_pretrained=False, frozen=False)
-    model.load_state_dict(torch.load(MODEL))
-    dorefa_prepare(model, inplace=True)
-    model.to(device)
-
     test_loader = get_imagewoof_dataloader(DatasetSplit.TEST, num_workers=2)
     train_loader = get_imagewoof_dataloader(DatasetSplit.TRAIN, num_workers=6)
     val_loader = get_imagewoof_dataloader(DatasetSplit.VAL, num_workers=6)
     criterion = torch.nn.CrossEntropyLoss()
 
-    print("Before QAT")
-    test_loss, test_acc = evaluate(model, test_loader, criterion, device)
-    print(f"{test_loss=} {test_acc=}")
+    def qat() -> None:
+        model = create_model(from_pretrained=False, frozen=False)
+        model.load_state_dict(torch.load(MODEL))
+        dorefa_prepare(model, inplace=True)
+        model.to(device)
 
-    print("QAT")
-    with SummaryWriter(log_dir=MODEL.parent / "dorefa") as writer:
-        optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.5, patience=5)
-        scaler = torch.amp.GradScaler(torch.device(device).type)
-        early_stopping = EarlyStopping(patience=10)
-        for epoch in range(1, QAT_MAX_EPOCHS + 1):
-            print(f"Epoch {epoch}")
-            train_loss, train_acc = train_one_epoch(model, train_loader, criterion, optimizer, device, scaler=scaler)
-            val_loss, val_acc = evaluate(model, val_loader, criterion, device)
-            writer.add_scalar("train/loss", train_loss, epoch)
-            writer.add_scalar("train/accuracy", train_acc, epoch)
-            writer.add_scalar("val/loss", val_loss, epoch)
-            writer.add_scalar("val/accuracy", val_acc, epoch)
-            if early_stopping(val_loss, model) and epoch >= QAT_MIN_EPOCHS:
-                print("Early stopping")
-                break
-            scheduler.step(val_loss)
-            print(f"Learning rate: {scheduler.get_last_lr()}")
+        print("Before QAT")
+        test_loss, test_acc = evaluate(model, test_loader, criterion, device)
+        print(f"{test_loss=} {test_acc=}")
 
-    print("After QAT")
-    test_loss, test_acc = evaluate(model, test_loader, criterion, device)
-    print(f"{test_loss=} {test_acc=}")
+        print("QAT")
+        with SummaryWriter(log_dir=MODEL.parent / "dorefa") as writer:
+            optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+            scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", factor=0.5, patience=5)
+            scaler = torch.amp.GradScaler(torch.device(device).type)
+            early_stopping = EarlyStopping(patience=10)
+            for epoch in range(1, QAT_MAX_EPOCHS + 1):
+                print(f"Epoch {epoch}")
+                train_loss, train_acc = train_one_epoch(model, train_loader, criterion, optimizer, device, scaler=scaler)
+                val_loss, val_acc = evaluate(model, val_loader, criterion, device)
+                writer.add_scalar("train/loss", train_loss, epoch)
+                writer.add_scalar("train/accuracy", train_acc, epoch)
+                writer.add_scalar("val/loss", val_loss, epoch)
+                writer.add_scalar("val/accuracy", val_acc, epoch)
+                if early_stopping(val_loss, model) and epoch >= QAT_MIN_EPOCHS:
+                    print("Early stopping")
+                    break
+                scheduler.step(val_loss)
+                print(f"Learning rate: {scheduler.get_last_lr()}")
 
-    torch.save(model.state_dict(), MODEL.with_stem(MODEL.stem + "_dorefa"))
+        print("After QAT")
+        test_loss, test_acc = evaluate(model, test_loader, criterion, device)
+        print(f"{test_loss=} {test_acc=}")
+
+        torch.save(model.state_dict(), MODEL.with_stem(MODEL.stem + "_dorefa"))
+
+    def test_qat() -> None:
+        model = create_model(from_pretrained=False, frozen=False)
+        dorefa_prepare(model, inplace=True)
+        model.load_state_dict(torch.load(MODEL.with_stem(MODEL.stem + "_dorefa")))
+        model.to(device)
+        test_loss, test_acc = evaluate(model, test_loader, criterion, device)
+        print(f"{test_loss=} {test_acc=}")
+
+    qat()
+    test_qat()
 
 
 if __name__ == "__main__":
