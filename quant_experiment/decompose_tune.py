96e022aa15bd64540da581732e851d4fc72576bd
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2025-03-23 21:18:12 +0000

    update

--- quant_experiment/decompose_tune.py
+++ quant_experiment/decompose_tune.py
@@ -21,7 +21,13 @@ def main():
         model.load_state_dict(torch.load(MODEL_PATH))
         model.to(device)
         decompose_model(model, trail, do_calculation=True)
+        num_params = sum(p.numel() for p in model.parameters())
+        trail.set_user_attr("num_params", num_params)
+
         test_loss, test_acc = evaluate(model, test_loader, criterion, device)
+        trail.set_user_attr("loss_after_decompose", test_loss)
+        trail.set_user_attr("acc_after_decompose", test_acc)
+
         return test_acc
 
     study_name = "decompose_tune"
@@ -32,7 +38,12 @@ def main():
         study_name=study_name,
         load_if_exists=True,
     )
-    study.optimize(objective, n_trials=100, n_jobs=1)
+    study.optimize(
+        objective,
+        n_trials=100,
+        n_jobs=1,
+        gc_after_trial=True,
+    )
 
 
 if __name__ == "__main__":
