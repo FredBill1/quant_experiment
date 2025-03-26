2cfc30daddb947bf567c26b91276cd148378e15e
FredBill1 <36622430+FredBill1@users.noreply.github.com>
2025-03-26 19:52:10 +0000

    update

---
+++ quant_experiment/quant_trt.py
@@ -0,0 +1,39 @@
+"https://nvidia.github.io/TensorRT-Model-Optimizer/guides/_pytorch_quantization.html"
+
+import modelopt.torch.opt as mto
+import modelopt.torch.quantization as mtq
+import torch
+import torch.nn as nn
+
+from .config import IMAGE_SIZE, MODEL_NAME, MODEL_PATH
+from .data.imagewoof import DatasetSplit, get_imagewoof_dataloader
+from .models import create_model
+from .utils.training import evaluate, get_device
+
+
+def main() -> None:
+    device = get_device()
+
+    model = create_model(MODEL_NAME)
+    model.load_state_dict(torch.load(MODEL_PATH))
+    model.to(device)
+
+    train_loader = get_imagewoof_dataloader(DatasetSplit.TRAIN, num_workers=0, persistent_workers=False)
+    test_loader = get_imagewoof_dataloader(DatasetSplit.TEST, num_workers=0, persistent_workers=False)
+
+    def forward_loop(model: nn.Module) -> None:
+        for batch in train_loader:
+            model(batch[0].to(device))
+
+    config = mtq.FP8_DEFAULT_CFG
+
+    model = mtq.quantize(model, config, forward_loop)
+    mtq.print_quant_summary(model)
+    mto.save(model, MODEL_PATH.with_stem("model_quant_trt"))
+    mtq.fold_weight(model)
+    test_loss, test_acc = evaluate(model, test_loader, nn.CrossEntropyLoss(), device)
+    print(f"{test_loss=}, {test_acc=}")
+
+
+if __name__ == "__main__":
+    main()
